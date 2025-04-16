1c1a62e41ff9c3f555043e4d8660efb2260d8f0c
Kirill <k.ved2008@mail.ru>
2025-04-16 19:29:16 +0300

    ready static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  target: 'static',
+  generate: {
+    fallback: true
+  },
   devtools: { enabled: true },
   modules: ['@nuxt/fonts','@nuxt/ui' ],
   nitro: {
